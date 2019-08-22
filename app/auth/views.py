10c86b44b4b9e9c0a0b5c9d74c9d2d7e978a678e
philipkariuki <kariukiphil@gmail.com>
2019-08-22 13:19:01 +0300

    creates models Staffs Assets Owners Routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,redirect,url_for,flash,request
 from . import auth
-from ..models import User
+from ..models import Owners, Assets, Staffs, Routes
 from .forms import LoginForm,RegistrationForm
 from .. import db
 from flask_login import login_user,logout_user,login_required
