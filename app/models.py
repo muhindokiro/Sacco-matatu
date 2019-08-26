fe3b72f753dd0edc01ce18523894e5c82fb8df21
muhindokiro <blaize1143@gmail.com>
2019-08-26 11:10:23 +0300

    add class owners create tables name,phone,email,asset

--- app/models.py
+++ app/models.py
@@ -9,3 +9,23 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.contrib.fileadmin import FileAdmin
 import os.path as op
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Staffs.query.get(int(user_id))
+    
+class Owners(UserMixin, db.Model):
+    __tablename__ = 'owners'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), index=True)
+    phone = db.Column(db.Integer, unique=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    date_added = db.Column(db.DateTime, default=datetime.now)
+    asset = db.relationship('Assets', backref='owners', lazy=True)
+   
+  
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+    def __repr__(self):
+        return f'Owners {self.name}'
