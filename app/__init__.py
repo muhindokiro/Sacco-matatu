82f8eda02c2547b4318ef86b4dfb61e0eb9f34ae
muhindokiro <blaize1143@gmail.com>
2019-08-28 14:40:16 +0300

    registered blueprints for the api

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+from flask_admin import Admin
+from flask_simplemde import SimpleMDE
+from flask_cors import CORS, cross_origin
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -14,10 +17,13 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos',IMAGES)
 mail = Mail()
+simple = SimpleMDE()
+admin = Admin()
 
 def create_app(config_name):
 
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
 
     # Creating the app   configurations
     app.config.from_object(config_options[config_name])
@@ -27,13 +33,21 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    simple.init_app(app)
+    admin.init_app(app)
 
-     # Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+    # Registering the blueprint
+    from .blueprints import api_bp
+    
+    app.register_blueprint(api_bp,url_prefix='/api')
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+#      # Registering the blueprint
+#     from .main import main as main_blueprint
+#     app.register_blueprint(main_blueprint)
+
+    # from .auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
      # configure UploadSet
     configure_uploads(app,photos)
