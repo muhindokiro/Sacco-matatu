17344fee8d6f6c9121c87849312908e2399ee073
muhindokiro <blaize1143@gmail.com>
2019-08-28 14:36:18 +0300

    created app instances for app,manage,migrate

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from app.models import Owner
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -22,7 +22,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,Owner = Owner )
 
 if __name__ == '__main__':
     manager.run()
