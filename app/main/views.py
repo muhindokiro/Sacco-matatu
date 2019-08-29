08867b188866a136a751d3ae860d882656371413
philipkariuki <kariukiphil@gmail.com>
2019-08-29 10:40:28 +0300

    updates views.py

--- app/main/views.py
+++ app/main/views.py
@@ -88,11 +88,6 @@ def philapi():
     
 
 
-# def dict_factory(cursor, row):
-#     d = {}
-#     for idx, col in enumerate(cursor.description):
-#         d[col[0]] = row[idx]
-#     return d
 
 
 # @main.route('/api/v1/resources/owners/all', methods=['GET'])
